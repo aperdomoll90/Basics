f433c953caf6c702c03667c06fcb02b3f728a684
aperdomoll90 <aperdomoll90@gmail.com>
2021-04-12 09:01:50 -0400

    got the corse mode translator to work

---
+++ practice.js
@@ -0,0 +1,36 @@
+const morseCode = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
+ }
+const toBeTrans = 'sos';
+ const translation = (words) => {
+     return words.toUpperCase().split('').map(letter => {
+       return morseCode[letter] ? morseCode[letter] : letter
+
+ }).join(' ')
+};
+
+     console.log(translation(toBeTrans))
