8476bc9f0194f4ab3a231c15808e9109c404649e
aperdomoll90 <aperdomoll90@gmail.com>
2021-04-16 10:06:05 -0400

    students who is not hired challange

---
+++ hired.js
@@ -0,0 +1,32 @@
+const students = [{
+name:'Vitoria',
+specialty:'Neuro',
+},
+{ 
+name:'Nikki',
+specialty:'Mfg',
+    },
+{
+name:'Rebecca',
+specialty:'retail',
+     }
+]
+
+const hired = ['Rebecca','Skar']
+
+// Comparison between two arrays with nested array methods
+// const availableCandidate = students.filter(stud => !hired.includes(stud.name))
+
+
+// console.log(availableCandidate)
+
+
+
+// Todds way
+// function getCandidates(students){
+//    return students.filter(student => student.hired !== 'true')
+//}
+
+// My way
+const hiredList = students.filter(student => student.hired !== 'true')
+ console.log(hiredList)
