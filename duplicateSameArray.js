ecca851776f5efb6256b05e5a9de13a1addbe69f
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2021-06-28 21:25:47 -0400

    remove duplicates from same array added

---
+++ duplicateSameArray.js
@@ -0,0 +1,14 @@
+const findDups = (arr) => {
+    const dups =[]
+    const compare =[]
+    for(let num of arr ){
+      if(!compare.includes(num)){
+        compare.push(num)
+      }else{
+        dups.push(num)
+      }
+    }
+    return compare
+  }
+  const arr = [4,3,2,1,4,5,6,7,7,8]
+  console.log(findDups(arr))
